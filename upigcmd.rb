be0b807ec9f79407fe86a920a882246d7e12cb9c
xiangwei <31531640@qq.com>
2010-04-27 11:45:58 +0800

    改进自动编码识别

--- upigcmd.rb
+++ upigcmd.rb
@@ -2,6 +2,22 @@
 
 require 'rubygems'
 require 'optparse'
+require 'jcode'
+require 'iconv'  
+require 'rchardet'
+
+class String  
+  def to_gbk(src_encoding='UTF-8')
+    return self if src_encoding.upcase.strip=='GBK'
+    Iconv.iconv("GBK//IGNORE","#{src_encoding}//IGNORE",self).to_s  
+  end  
+  def to_utf8(src_encoding='GBK')
+    return self if src_encoding.upcase.strip=='UTF-8'
+    Iconv.iconv("UTF-8//IGNORE","#{src_encoding}//IGNORE",self).to_s  
+  end  
+end 
+
+
 exit if Object.const_defined?(:Ocra)
 
 options = {}
@@ -39,10 +55,28 @@ optparse.parse!
 keyword = ARGV.join(' ') 
 
 $stderr.puts optparse if keyword.strip==''
+
+
 #$stderr.puts "options"
 
-File.open(options[:temp], 'w'){|f|
-  #f.write(optparse.to_s)
+
+
+    #Iconv.iconv("UTF-8//IGNORE","GBK//IGNORE",self).to_s  
+
+#convert txt to html
+#f.write(optparse.to_s)
+File.open(options[:temp], 'w'){|temp_file|
+  File.open(options[:output], 'w'){|out_file|
+    File.open(keyword, 'r'){|f|
+      src_str = f.read
+      #(confidence, encoding)= CharDet.detect(src_str)
+      encode_det = CharDet.detect(src_str)
+      encoding = encode_det['encoding'].upcase
+      confidence = encode_det['confidence']
+      $stderr.puts '编码不能正确识别' if confidence <0.1
+      temp_file.print src_str.to_utf8(encoding)
+    }
+  }
 }
 
 #$filetypes='*.{txt}'
@@ -51,7 +85,6 @@ File.open(options[:temp], 'w'){|f|
 
 #puts options
 
-#convert txt to html
 
 #convert html to prc
 
