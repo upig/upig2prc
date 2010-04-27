89cdc7e1af1f98792415ab8251dd215ec028aa68
xiangwei <31531640@qq.com>
2010-04-27 10:21:11 +0800

    加入参数，未测试临时版本

--- upig2prc.rb
+++ upig2prc.rb
@@ -1,12 +1,49 @@
-#require 'rubygems'
+#xiangwei 31531640@qq.com
+
+require 'rubygems'
+require 'optparse'
 exit if Object.const_defined?(:Ocra)
 
-puts 'dkdkd'
-`pause`
-puts `temp/kindlegen.exe --help`
+options = {}
 
-puts File.exist?('upig2prc.rb')
+optparse = OptionParser.new do|opts|
+  # Set a banner, displayed at the top
+  # of the help screen.
+  opts.banner =<<'EOF'
+批量转换txt文件为prc文档(Kindle使用）
+1. 自动排版txt文件
+2. 自动调整prc格式
 
-puts ARGV
+使用方法:
+  upig2prc [options] [dirname]"
+使用示例:
+  upig2prc          #will convert all txt in current directory
+  upig2prc d:/books #will convert all txt in d:/books
+EOF
+
+  options[:n] = false
+  opts.on( '-n', '', '不要遍历子文件夹') do
+    options[:one] = true
+  end
+
+  opts.on( '-h', '', '帮助' ) do
+    puts opts
+    exit
+  end
+end
+
+optparse.parse!
+
+keyword = ARGV.join(' ') 
+
+$filetypes='*.{txt}'
+
+#temp_path 
+#参考那个临时文件，好好改一下代码，可以生成比较漂亮的kindle文件
+Dir.glob("**/#{$filetypes}").each {|fileName|
+  #convert txt to html
+  #convert html to prc
+  #puts `temp/kindlegen.exe #{fileName}`
+}
 
 `pause`
